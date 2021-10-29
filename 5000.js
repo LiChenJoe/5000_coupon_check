949ffec4bfa5ac041f5dbe0bc2211387ff404ea9
LiChenJoe <92890077+LiChenJoe@users.noreply.github.com>
2021-10-29 17:59:53 +0800

    Add files via upload

--- 5000.js
+++ 5000.js
@@ -329,6 +329,11 @@ function check() {
       showPrize.textContent ="🀄 第一週 地方創生券(週五)";
       showPrize2.textContent ="🀄 第三週 國旅券(週二)";
       showWarn.textContent ="跨週中籤超級幸運！快去買樂透吧！";
+    } else if (Number(guess) === Number(lucky335)) {
+      showResult.textContent = "恭喜抽到";
+      showPrize.textContent ="🀄 第一週 地方創生券(週五)";
+      showPrize2.textContent ="🀄 第三週 國旅券(週二)";
+      showWarn.textContent ="跨週中籤超級幸運！快去買樂透吧！";
     } else if (Number(guess) === Number(lucky79)) {
       showResult.textContent = "恭喜抽到";
       showPrize.textContent ="🀄 第一週 藝FUN券-紙本(週三) ";
